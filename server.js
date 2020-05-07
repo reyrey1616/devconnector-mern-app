6733d4373d51cc9461ebc02c3eef103a0f89a876
yobbad16 <dabboyrey@gmail.com>
2020-05-08 02:47:03 +0800

    Created routes directory and created routes for every module and define it to server

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const colors = require('colors');
 const connectDB = require('./config/db');
+
+// Route imports
+
+// Initialize Express
 const app = express();
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Connect to database
 connectDB();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
-});
+// Listen to port
 app.listen(PORT, () =>
   console.log(`SERVER RUNNING ON PORT ${PORT}`.green.inverse)
 );
