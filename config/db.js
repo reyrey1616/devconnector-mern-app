1bb2318775eba8b3c507c54f4725143babfad115
yobbad16 <dabboyrey@gmail.com>
2020-05-08 02:19:31 +0800

    Created a database connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const colors = require('colors');
+const db = config.get('MONGO_URI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected'.green.inverse);
+  } catch (err) {
+    console.error(err.message.red.inverse);
+    //     Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const colors = require('colors');
+const connectDB = require('./config/db');
 const app = express();
 
+// Connect to database
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
