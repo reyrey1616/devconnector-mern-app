235a68616d87f85993fcda2c486f2139a45e37f4
yobbad16 <dabboyrey@gmail.com>
2020-05-08 03:55:27 +0800

    Added token signing in register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 
@@ -31,7 +33,9 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'user already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user already exists' }] });
       }
 
       const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
@@ -47,12 +51,24 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
+
+      const payload = { user: { id: user.id } };
+
+      jwt.sign(
+        payload,
+        config.get('JWT_SECRET'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message.red.inverse);
       res.status(500).send('Server Error');
     }
-
-    res.send('User Registered');
   }
 );
 
