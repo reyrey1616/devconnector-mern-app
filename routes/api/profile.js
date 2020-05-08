495e1b0585f68ba4059763afca6032c6ffde6c74
yobbad16 <dabboyrey@gmail.com>
2020-05-08 15:57:26 +0800

    Created a route for getting single profile using the user id as url params

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -121,4 +121,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get single profile by user id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatart']);
+
+    if (!profile) {
+      res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.status(200).json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
