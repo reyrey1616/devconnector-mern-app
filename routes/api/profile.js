0cdd2d43433f3f0c82772e5d8f40ed217e04fa46
yobbad16 <dabboyrey@gmail.com>
2020-05-09 00:56:35 +0800

    Created route for getting github repository based on github username using request package of node for send api request in the backend

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -307,6 +309,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 
     res.status(200).json(profile);
   } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'GITHUB_CLIENT_ID'
+      )}&client_secret=${config.get('GITHUB_SECRET')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: options.uri });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
