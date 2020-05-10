a9d30ecb9e7a5adc4508bb71cb73c0cbddfda47e
yobbad16 <dabboyrey@gmail.com>
2020-05-11 03:00:28 +0800

    Added the action and types to the profile reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -10,9 +10,10 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
-    //     case SET_ALERT:
-    //       return [...state, payload];
-
+    case GET_PROFILE:
+      return { ...state, profile: payload, loading: false };
+    case PROFILE_ERROR:
+      return { ...state, error: payload, loading: false };
     default:
       return state;
   }
