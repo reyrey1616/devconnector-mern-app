6c87add48f6b67ac531ce062d077a0ebc989fc2e
yobbad16 <dabboyrey@gmail.com>
2020-05-11 02:03:42 +0800

    Created logics in navbar to display different links depends on authentication

--- client/src/components/layout/navbar.component.jsx
+++ client/src/components/layout/navbar.component.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
+
 import { Link } from 'react-router-dom';
-const Navbar = () => (
-  <nav className='navbar bg-dark'>
-    <h1>
-      <Link to='/'>
-        <i className='fas fa-code'></i> DevConnector
-      </Link>
-    </h1>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href='!#'>
+          <i className='fas fa-sign-out-alt'> </i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
     <ul>
       <li>
         <Link to='/'>Developers</Link>
@@ -18,6 +28,27 @@ const Navbar = () => (
         <Link to='/login'>Login</Link>
       </li>
     </ul>
-  </nav>
-);
-export default Navbar;
+  );
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code'></i> DevConnector
+        </Link>
+      </h1>
+
+      {!loading && (
+        <Fragment> {isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { logout })(Navbar);
