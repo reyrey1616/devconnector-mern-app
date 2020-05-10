f8b6efcdb8991963af689660203e247d1755f089
yobbad16 <dabboyrey@gmail.com>
2020-05-11 03:12:31 +0800

    Bring the get current profile in dashboard component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,7 @@ import { GET_PROFILE, PROFILE_ERROR } from './types';
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/profile/me');
-
+    console.log(res.data);
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
