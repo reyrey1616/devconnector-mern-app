8ef34f6b62f61a192f0e8ecfbc23f60a4d02b3fd
yobbad16 <dabboyrey@gmail.com>
2020-05-10 17:49:24 +0800

    Created login and register component without content and added it to the route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/navbar.component';
-import Landing from './components/layout/landing.component';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/layout/navbar.component';
+import Landing from './components/layout/landing.component';
+
+import Register from './components/auth/register.component';
+import Login from './components/auth/login.component';
 
 const App = () => (
   <Fragment>
     <Router>
       <Navbar />
       <Route path='/' exact component={Landing} />
+
+      <section className='container'>
+        <Switch>
+          <Route path='/login' exact component={Login} />
+          <Route path='/register' exact component={Register} />
+        </Switch>
+      </section>
     </Router>
   </Fragment>
 );
