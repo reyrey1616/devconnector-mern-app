15b97c70939fc80bc335bebb45b2a723c748fb58
yobbad16 <dabboyrey@gmail.com>
2020-05-11 01:31:44 +0800

    Added the isAuthenticated field from auth reducer to register and login component to determine if the user is logged in

--- client/src/components/auth/login.component.jsx
+++ client/src/components/auth/login.component.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
-
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -20,6 +19,11 @@ const Login = ({ login }) => {
     e.preventDefault();
     login(email, password);
   };
+
+  //   Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to='/' />;
+  }
   return (
     <React.Fragment>
       <h1 className='large text-primary'>Sign In</h1>
@@ -58,6 +62,11 @@ const Login = ({ login }) => {
 
 Login.propTypes = {
   login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { login })(Login);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
