c13cee074a39caebfe727b3a53121192854f384f
yobbad16 <dabboyrey@gmail.com>
2020-05-11 19:18:10 +0800

    Created add education action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -92,3 +92,38 @@ export const addExperience = () => async (dispatch) => {
     });
   }
 };
+
+// ADD EDUCATION
+
+export const addEducation = () => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/profile/education', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+    history.push('/dashboard');
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach((item) => dispatch(setAlert(item.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
