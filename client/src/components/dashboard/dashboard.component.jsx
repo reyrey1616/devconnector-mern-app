8219e7ff10207f88efc49504d327a15162d95c74
yobbad16 <dabboyrey@gmail.com>
2020-05-11 17:51:25 +0800

    Created a condition in dashboard that display the create profile button if the user profile has not been yet setup

--- client/src/components/dashboard/dashboard.component.jsx
+++ client/src/components/dashboard/dashboard.component.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/spinner.component';
@@ -23,7 +24,14 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment> has </Fragment>
       ) : (
-        <Fragment> has not</Fragment>
+        <Fragment>
+          {' '}
+          <p> You have not yet set up a profile, please add some info.</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            {' '}
+            Create Profile{' '}
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   );
