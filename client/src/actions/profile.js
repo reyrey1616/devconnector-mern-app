42cf9e570adada276750b99f884e4355faad2f13
yobbad16 <dabboyrey@gmail.com>
2020-05-12 19:01:02 +0800

    Created action to get all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
@@ -12,9 +13,30 @@ import {
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/profile/me');
-    console.log(res.data);
     dispatch({
       type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// GET ALL PROFILES
+export const getProfiles = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({
+      type: GET_PROFILES,
       payload: res.data,
     });
   } catch (error) {
@@ -197,3 +219,5 @@ export const deleteAccount = () => async (dispatch) => {
     }
   }
 };
+
+//
