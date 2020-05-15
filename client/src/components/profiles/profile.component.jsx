763da0ed154c3e4c72e29f4d41965f847bc638cf
yobbad16 <dabboyrey@gmail.com>
2020-05-15 20:07:18 +0800

    Created the profile component that hold single profile with all states but without jsx

---
+++ client/src/components/profiles/profile.component.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment, useEffect } from 'react';
+import Spinner from '../layout/spinner.component';
+import PropTypes from 'prop-types';
+import { connect } from 'reacr-redux';
+import { getProfiles } from '../../actions/profile';
+
+const Profile = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+  return <div></div>;
+};
+
+Profile.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profile);
