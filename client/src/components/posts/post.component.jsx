d641f742c528a3a6952dbb6944d5a44a3f3915b6
yobbad16 <dabboyrey@gmail.com>
2020-05-18 23:49:19 +0800

    Finalized the post component

--- client/src/components/posts/post.component.jsx
+++ client/src/components/posts/post.component.jsx
@@ -21,18 +21,23 @@ const PostItem = ({
         Posted on <Moment date={date} format='YYYY/MM/DD' />{' '}
       </p>
       <button type='button' className='btn btn-light'>
-        <i className='fas fa-thumbs-up'></i>
-        <span>{likes.length}</span>
+        <i className='fas fa-thumbs-up'></i>{' '}
+        {likes.length > 0 && <span>{likes.length}</span>}
       </button>
       <button type='button' className='btn btn-light'>
         <i className='fas fa-thumbs-down'></i>
       </button>
-      <a href='post.html' className='btn btn-primary'>
-        Discussion <span className='comment-count'>{comments.length}</span>
-      </a>
-      <button type='button' className='btn btn-danger'>
-        <i className='fas fa-times'></i>
-      </button>
+      <Link to={`/post/${_id}`} className='btn btn-primary'>
+        Discussion{' '}
+        {comments.length > 0 && (
+          <span className='comment-count'>{comments.length}</span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type='button' className='btn btn-danger'>
+          <i className='fas fa-times'></i>
+        </button>
+      )}
     </div>
   </div>
 );
