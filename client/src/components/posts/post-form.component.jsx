3f7d1c3425eea7d5eaa42041b6f6540f51d7fdbc
yobbad16 <dabboyrey@gmail.com>
2020-05-19 00:31:38 +0800

    Added the actions to the jsx of post form

--- client/src/components/posts/post-form.component.jsx
+++ client/src/components/posts/post-form.component.jsx
@@ -3,9 +3,34 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addPost } from '../../actions/post';
 const PostForm = ({ addPost }) => {
-  const [text, setText] = useState();
+  const [text, setText] = useState('');
 
-  return <div></div>;
+  return (
+    <div className='post-form'>
+      <div className='bg-primary p'>
+        <h3>Say Something...</h3>
+      </div>
+      <form
+        className='form my-1'
+        onSubmit={(e) => {
+          e.preventDefault();
+          addPost({ text });
+          setText('');
+        }}
+      >
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
+          value={text}
+          placeholder='Create a post'
+          onChange={(e) => setText(e.target.value)}
+          required
+        ></textarea>
+        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+      </form>
+    </div>
+  );
 };
 
 PostForm.propTypes = {
